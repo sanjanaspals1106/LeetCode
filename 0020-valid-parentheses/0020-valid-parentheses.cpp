132861764a2496fd2537929efd37d0096a5c240c
Sanjana Rama <sanjanaspals1106@gmail.com>
2026-06-22 20:43:48 +0530

    Time: 0 ms (100%), Space: 9 MB (37.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(char ch : s){
-            if (ch=='(' || ch=='{' || ch=='['){
+        for(char ch: s){
+            if(ch=='('||ch=='{'||ch=='['){
                 st.push(ch);
             }
-            else {
-            if(st.empty())  return false;
-            if (st.top()=='(' && ch==')'){
-                st.pop();
-            }
-            else if (st.top()=='{' && ch=='}'){
-                st.pop();
-            }
-            else if (st.top()=='[' && ch==']'){
-                st.pop();
-            }
-            else{return false;}
+            else{
+                if(st.empty()){return false;}
+                else if(st.top()=='(' && ch==')'){
+                    st.pop();
+                }
+                else if(st.top()=='[' && ch==']'){
+                    st.pop();
+                }
+                else if(st.top()=='{' && ch=='}'){
+                    st.pop();
+                }
+                else{return false;}
             }
         }
         return st.empty();
