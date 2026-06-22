687dfb2e7ef643c3651b375add6c5a63ec3e79e7
Sanjana Rama <sanjanaspals1106@gmail.com>
2026-06-22 20:13:08 +0530

    Time: 0 ms (100%), Space: 24 MB (18.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int pos=0;
         for(int i=0; i<nums.size(); i++){
-            if (nums[i]!=0){
+            if(nums[i]!=0){
                 nums[pos]=nums[i];
                 pos++;
             }
@@ -12,7 +12,6 @@ public:
             nums[pos]=0;
             pos++;
         }
-        
         
     }
 };
