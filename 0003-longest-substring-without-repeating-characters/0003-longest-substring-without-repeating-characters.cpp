4cb6201182db693c40717e3a17fe76c68ad4250c
Sanjana Rama <sanjanaspals1106@gmail.com>
2026-07-07 23:10:53 +0530

    Time: 7 ms (63.72%), Space: 11.9 MB (55.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,14 +4,13 @@ public:
         unordered_map<char,int> mp;
         int count=0;
         int i=0, j=0;
-        while(j<s.size()){
+        for(int j=0; j<s.size(); j++){
             mp[s[j]]++;
             while(mp[s[j]]>1){
                 mp[s[i]]--;
                 i++;
             }
             count=max(count, j-i+1);
-            j++;
         }
         return count;
     }
