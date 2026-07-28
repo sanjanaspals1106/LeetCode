8b9cdb9be6ceae77082b90ac3c39a62d7cc0d644
Sanjana Rama <sanjanaspals1106@gmail.com>
2026-07-28 20:03:04 +0530

    Time: 0 ms (100%), Space: 15.3 MB (6.81%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> sol;
+        for(int i=0; i<matrix[0].size(); i++){
+            vector<int> row;
+            for(int j=0; j<matrix.size(); j++){
+                row.push_back(matrix[j][i]);
+            }
+            sol.push_back(row);
+        }
+        return sol;
+    }
+};
